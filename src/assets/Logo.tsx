c9e3cfc8846191771746f28bd795671cc6467c30
MishaKollen <mikhailkollen@gmail.com>
2023-05-12 17:18:30 +0400

    optimize for mobile

--- src/assets/Logo.tsx
+++ src/assets/Logo.tsx
@@ -1,6 +1,6 @@
 const Logo = () => {
   return (
-    <svg width="63" height="31" viewBox="0 0 63 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width="63" height="31" viewBox="0 0 63 31" fill="none" id="logo" xmlns="http://www.w3.org/2000/svg">
 <path d="M19.1861 28.363L34.2771 0.539673H37.5551V30.5217H35.3964V3.07816L20.3254 30.5217H18.0068L2.93581 3.07816V30.5217H0.7771V0.539673H4.05514L19.1861 28.363Z" fill="white"/>
 <path d="M51.6468 13.4105L63 30.4603H60.3616L50.0877 15.2494L37.2954 30.4603H35.4165V0.478271H37.6552V26.8025L59.8819 0.478271H62.5203L51.6468 13.4105Z" fill="white"/>
 </svg>
