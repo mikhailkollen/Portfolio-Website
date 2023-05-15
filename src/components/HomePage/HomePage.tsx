ca203578703c74c5ff4bf422bcf8e6f27b40ccd3
MishaKollen <mikhailkollen@gmail.com>
2023-05-15 12:10:30 +0400

    fix scroll behavior

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,62 +1,61 @@
-import Logo from '../../assets/Logo'
-import './HomePage.css'
-import WhiteCircleIcon from '../../assets/WhiteCircleIcon'
+import Logo from '../../assets/Logo';
+import './HomePage.css';
+import WhiteCircleIcon from '../../assets/WhiteCircleIcon';
 import Spline from '@splinetool/react-spline';
 import { useEffect, useState } from 'react';
-import Hamburger from 'hamburger-react'
+import Hamburger from 'hamburger-react';
+import { Link } from 'react-scroll';
 
 const HomePage = () => {
-  const [isMobile, setIsMobile] = useState(false)
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
-  const [navLinkColor, setNavLinkColor] = useState('')
+  const [isMobile, setIsMobile] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [navLinkColor, setNavLinkColor] = useState('');
 
   useEffect(() => {
     if (window.innerWidth < 700) {
-      setIsMobile(true)
+      setIsMobile(true);
     } else {
-      setIsMobile(false)
-      
+      setIsMobile(false);
     }
-  }, [])
+  }, []);
 
   window.addEventListener('resize', () => {
     if (window.innerWidth < 700) {
-      setIsMobile(true)
+      setIsMobile(true);
     } else {
-      setIsMobile(false)
+      setIsMobile(false);
     }
-    setIsMenuOpen(false)
-  })
-
-
+    setIsMenuOpen(false);
+  });
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll)
-    return () => window.removeEventListener('scroll', handleScroll)
-  }, [])
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
   const handleScroll = () => {
     if (window.scrollY > 300 && window.innerWidth < 700) {
-      setNavLinkColor('#130a25')
+      setNavLinkColor('#130a25');
     } else {
-      setNavLinkColor('')
+      setNavLinkColor('');
     }
-  }
-
-
-
+  };
 
   return (
-    <section className='home-page'> 
-    {/* REMOVE OVERLAY UNTIL FIND A BETTER SOLUTION AS ON MOBILE THE SIDEBAR IS CLOSED WHEN SCROLLING */}
-      {/* {isMenuOpen && <div className="overlay" onClick={()=>setIsMenuOpen(false)}></div>} */}
+    <section className='home-page'>
       <header className='home-page-header'>
         <Logo />
         {isMobile && <Hamburger color='#fff' toggled={isMenuOpen} toggle={setIsMenuOpen} />}
-        <div className={`header-links${isMobile ? "-mobile" : ""}`} style={{ transform: (isMobile) ? isMenuOpen ? "translateX(0)" : "translateX(100%)" : undefined}}>
-          <a className="nav-link" href="#about-me" title='About' style={{ color: navLinkColor }}>About me</a>
-          <a className="nav-link" href="#projects" title='Projects' style={{ color: navLinkColor }}>Projects</a>
-          <a className="nav-link" href="#contacts" title='Contacts' style={{ color: navLinkColor }}>Contacts</a>
+        <div className={`header-links${isMobile ? '-mobile' : ''}`} style={{ transform: isMobile ? (isMenuOpen ? 'translateX(0)' : 'translateX(100%)') : undefined }}>
+          <Link className="nav-link" to="about-me" smooth={true} duration={300} offset={-100} style={{ color: navLinkColor }}>
+            About me
+          </Link>
+          <Link className="nav-link" to="projects" smooth={true} duration={300} offset={-100} style={{ color: navLinkColor }}>
+            Projects
+          </Link>
+          <Link className="nav-link" to="contacts" smooth={true} duration={300} offset={-100} style={{ color: navLinkColor }}>
+            Contacts
+          </Link>
         </div>
       </header>
       <h1 className='heading-name'>MIKHAIL <br /> KOLLEN</h1>
@@ -74,10 +73,8 @@ const HomePage = () => {
         <span className='horizontal-line'></span>
       </div>
       <Spline className="spline-container" scene="https://prod.spline.design/mrm3q2O4nROI3clV/scene.splinecode" />
-
-
     </section>
-  )
-}
+  );
+};
 
 export default HomePage
