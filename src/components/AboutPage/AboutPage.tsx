56abaee6b50b00a700172c7c2c2705dd094909ad
Mikhail Kollen <mikhailkollen@gmail.com>
2023-08-10 18:49:12 +0400

    update AboutPage

--- src/components/AboutPage/AboutPage.tsx
+++ src/components/AboutPage/AboutPage.tsx
@@ -1,58 +1,88 @@
-import './AboutPage.css'
-import EightyPercentBar from '../../assets/EightyPercentBar'
-import NinetyPercentBar from '../../assets/NinetyPercentBar'
-import SixtyPercentBar from '../../assets/SixtyPercentBar'
-
+import "./AboutPage.css";
+import EightyPercentBar from "../../assets/EightyPercentBar";
+import NinetyPercentBar from "../../assets/NinetyPercentBar";
+import SixtyPercentBar from "../../assets/SixtyPercentBar";
 
 const AboutPage = () => {
   return (
-    <section className='about-page' id="about-me">
+    <section className="about-page" id="about-me">
       <div className="about-page-info-container">
         <div className="about-page-info">
-          <h2 className='about-page-heading'>
-            ABOUT ME
-          </h2>
-          <p className='about-page-text'>
-            I am a career changer with over 4 years of experience in teaching, client service, and support. At the moment
-            I am focused on acquiring a Frontend Developer position. I have created several personal projects, including a
-            serverless e-commerce website and multiple SPAs using React.js. It allowed me to become confident in building
-            applications using Vanilla JavaScript and React.js.
-            Currently, I am a Front-end Developer Intern at Quantori, where I utilize my skills in Redux.js, TypeScript,
-            JavaScript, and React.js to create responsive and user-friendly web applications. I also have a wide experience in
-            project management and problem-solving.
-            I am committed to continuous learning and seeking opportunities to expand my knowledge and apply my new
-            skills.
+          <h2 className="about-page-heading">ABOUT ME</h2>
+          <p className="about-page-text">
+            I am a seasoned Front-end Developer with over a year of hands-on
+            experience in Web Development. Through this time, I have conceived
+            and executed several personal projects. Notably, I crafted a
+            serverless e-commerce website using JS, React, Redux, and more,
+            alongside a protein search app that utilizes authentication and
+            dynamic application state, built with TS, React, Redux, and
+            Firebase, among other technologies.{" "}
+          </p>{" "}
+          <p className="about-page-text">
+            {" "}
+            My proficiency extends to crafting multiple SPAs utilizing React.js,
+            thus solidifying my confidence in working with Vanilla JavaScript,
+            TypeScript, React.js, and Redux. Currently, I thrive as a Software
+            Engineer at Quantori, a prominent player in the Life Science and
+            Healthcare sector. Here, I contribute by enhancing the Front-end
+            aspects of client products, ensuring they excel in adaptability,
+            responsiveness, and scalability.{" "}
+          </p>{" "}
+          <p className="about-page-text">
+            {" "}
+            My journey includes valuable experience in teaching and intricate
+            problem-solving. I am unceasingly dedicated to advancing my
+            knowledge and skill set, seeking continuous learning and
+            opportunities to apply my evolving expertise.
           </p>
         </div>
         <div className="about-page-skills">
           <span>
-            <h3 className='skills-title'>
-              JAVASCRIPT
-            </h3>
+            <h3 className="skills-title">JAVASCRIPT</h3>
             <EightyPercentBar></EightyPercentBar>
           </span>
           <span>
-            <h3 className='skills-title'>
-              HTML/CSS
-            </h3>
+            <h3 className="skills-title">HTML/CSS</h3>
             <NinetyPercentBar></NinetyPercentBar>
           </span>
           <span>
-            <h3 className='skills-title'>
-              REACT
-            </h3>
+            <h3 className="skills-title">REACT</h3>
             <SixtyPercentBar></SixtyPercentBar>
           </span>
         </div>
       </div>
-      <div className="about-page-projects" id='projects'>
-        <a href='https://github.com/mikhailkollen/react-ecommerce-project' target='_blank' className="project-title">E-STORE</a>
-        <a href='https://github.com/mikhailkollen/react-to-do-app' target='_blank' className="project-title">TO-DO APP</a>
-        <a href='https://github.com/mikhailkollen/react-ip-address-tracker-app' target='_blank' className="project-title">IP-TRACKER</a>
-        <a href='https://github.com/mikhailkollen/Calculator-Project' target='_blank' className="project-title">CALCULATOR</a>
+      <div className="about-page-projects" id="projects">
+        <a
+          href="https://github.com/mikhailkollen/react-ecommerce-project"
+          target="_blank"
+          className="project-title"
+        >
+          E-STORE
+        </a>
+        <a
+          href="https://github.com/mikhailkollen/react-to-do-app"
+          target="_blank"
+          className="project-title"
+        >
+          TO-DO APP
+        </a>
+        <a
+          href="https://github.com/mikhailkollen/react-ip-address-tracker-app"
+          target="_blank"
+          className="project-title"
+        >
+          IP-TRACKER
+        </a>
+        <a
+          href="https://github.com/mikhailkollen/Calculator-Project"
+          target="_blank"
+          className="project-title"
+        >
+          CALCULATOR
+        </a>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default AboutPage
+export default AboutPage;
